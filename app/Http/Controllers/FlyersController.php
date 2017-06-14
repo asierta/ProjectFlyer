8e8aa578b7e063ec02c607b6055de98a0503d138
Asier <tamasasier@gmail.com>
2017-06-14 14:19:45 +0200

    update

---
+++ app/Http/Controllers/FlyersController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FlyersController extends Controller
+{
+    public function create(){
+    	return view('flyers.create');
+    }
+}
